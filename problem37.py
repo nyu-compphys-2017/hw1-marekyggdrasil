660b6ab52f6b7b0c54c6f79d4773f4022534b7d8
marek <marek.narozniak@gmail.com>
2017-09-16 13:59:00 -0400

    implemented command line handling

--- problem37.py
+++ problem37.py
@@ -1,27 +1,42 @@
+import argparse
 import numpy as np
 import sysconfig
 import sys
 import matplotlib.pyplot as plt
 
-N = 300
-maxiter = 1000
-step = 4.0 / N
+def actionGenerate(N, maxiter) :
+    step = 4.0 / N
+    rx = []
+    ry = []
+    for i in np.arange(-2.0, 2.0, step) :
+        for j in np.arange(-2.0, 2.0, step) :
+            c = complex(i, j)
+            z = complex(0.0, 0.0) + c
+            k = 0
+            while k < maxiter and np.absolute(z) < 2.0 :
+                z = z**2 + c
+                k += 1
+            if k == maxiter :
+                rx.append(z.real)
+                ry.append(z.imag)
+    return rx, ry
+
+def actionPlot(b, rx, ry) :
+    heatmap, xedges, yedges = np.histogram2d(rx, ry, bins=b)
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    plt.clf()
+    plt.imshow(heatmap.T, extent=extent, origin='lower')
+    plt.show()
+
+parser = argparse.ArgumentParser(description='Mandelbrot set generator and plotter.')
+parser.add_argument('--generate', nargs=2, metavar=('N', 'maxiter'), type=int, help='generate Mandelbrot set from NxN grid and at most maxiter iterations per point')
+parser.add_argument('--plot', nargs=1, metavar=('b'), type=int, help='plot mandelbrot set with b bins')
+args = parser.parse_args()
+
 rx = []
 ry = []
-for i in np.arange(-2.0, 2.0, step) :
-    for j in np.arange(-2.0, 2.0, step) :
-        c = complex(i, j)
-        z = complex(0.0, 0.0) + c
-        k = 0
-        while k < maxiter and np.absolute(z) < 2.0 :
-            z = z**2 + c
-            k += 1
-        if k == maxiter :
-            rx.append(z.real)
-            ry.append(z.imag)
+if args.generate is not None :
+    rx, ry = actionGenerate(args.generate[0], args.generate[1])
 
-heatmap, xedges, yedges = np.histogram2d(rx, ry, bins=100)
-extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-plt.clf()
-plt.imshow(heatmap.T, extent=extent, origin='lower')
-plt.show()
+if args.plot is not None :
+    actionPlot(args.plot[0], rx, ry)
